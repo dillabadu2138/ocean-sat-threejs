2384f674eea29de7f5f4848c0d53d1f48b44dd03
dillabadu2138 <dillabadu2138@gmail.com>
2023-03-07 15:30:24 +0900

    refactor a procedural code into an object-oriented one

--- src/script.js
+++ src/script.js
@@ -1,39 +1,76 @@
 import './styles.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// setup scene, camera and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 2;
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+class Demo {
+  constructor() {
+    this.initialize();
+  }
 
-// create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  initialize() {
+    // create a webgl renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-window.addEventListener('resize', onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
+    // add the renderer to our HTML document
+    document.body.appendChild(this.renderer.domElement);
+
+    // add an window resize event listener
+    window.addEventListener('resize', this.onWindowResize, false);
+
+    // create a camera
+    const fov = 75;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 1.0;
+    const far = 1000.0;
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera.position.set(0, 0, 2);
+
+    // create a control
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    // create a scene
+    this.scene = new THREE.Scene();
+
+    // create a cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true,
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    //
+    this.animate();
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  // create a loop that causes the renderer to draw the scene every time the screen is refreshed
+  animate() {
+    requestAnimationFrame(() => {
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
+
+      this.renderer.render(this.scene, this.camera);
+
+      this.animate();
+    });
+  }
 }
 
-// create a loop that causes the renderer to draw the scene every time the screen is refreshed
-function animate() {
-  requestAnimationFrame(animate);
+let APP = null;
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
+function main() {
+  APP = new Demo();
 }
 
-animate();
+main();
