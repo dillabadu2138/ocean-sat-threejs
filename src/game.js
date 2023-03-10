f3ebfd417670ecd5533bf0178dbefcb5111700e3
dillabadu2138 <dillabadu2138@gmail.com>
2023-03-10 11:08:26 +0900

    create a game module to abstract away the threejs internals

---
+++ src/game.js
@@ -0,0 +1,55 @@
+import * as THREE from 'three';
+
+export class Game {
+  constructor() {
+    this.#initialize();
+  }
+
+  #initialize() {
+    // create a webgl renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // add the renderer to our HTML document
+    document.body.appendChild(this.renderer.domElement);
+
+    // add an window resize event listener
+    window.addEventListener('resize', () => this.onWindowResize(), false);
+
+    // create a camera
+    const fov = 75;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 1.0;
+    const far = 1000.0;
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera.position.set(0, 0, 2);
+
+    // create a scene
+    this.scene = new THREE.Scene();
+
+    // call overriden method from child
+    this.onInitialize();
+
+    //
+    this.animate();
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  // create a loop that causes the renderer to draw the scene every time the screen is refreshed
+  animate() {
+    requestAnimationFrame(() => {
+      this.cube.rotation.x += 0.01;
+      this.cube.rotation.y += 0.01;
+
+      this.renderer.render(this.scene, this.camera);
+
+      this.animate();
+    });
+  }
+}
--- src/script.js
+++ src/script.js
@@ -2,38 +2,20 @@ import './styles.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-class Demo {
+import { Game } from './game.js';
+
+class Demo extends Game {
   constructor() {
-    this.initialize();
+    // calls the parent class's constructor
+    super();
   }
 
-  initialize() {
-    // create a webgl renderer
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // add the renderer to our HTML document
-    document.body.appendChild(this.renderer.domElement);
-
-    // add an window resize event listener
-    window.addEventListener('resize', this.onWindowResize, false);
-
-    // create a camera
-    const fov = 75;
-    const aspect = window.innerWidth / window.innerHeight;
-    const near = 1.0;
-    const far = 1000.0;
-    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this.camera.position.set(0, 0, 2);
-
+  onInitialize() {
     // create a control
     const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.enableDamping = true;
     controls.target.set(0, 0, 0);
     controls.update();
-
-    // create a scene
-    this.scene = new THREE.Scene();
 
     // create a cube
     const geometry = new THREE.BoxGeometry();
@@ -43,27 +25,6 @@ class Demo {
     });
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
-
-    //
-    this.animate();
-  }
-
-  onWindowResize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-
-  // create a loop that causes the renderer to draw the scene every time the screen is refreshed
-  animate() {
-    requestAnimationFrame(() => {
-      this.cube.rotation.x += 0.01;
-      this.cube.rotation.y += 0.01;
-
-      this.renderer.render(this.scene, this.camera);
-
-      this.animate();
-    });
   }
 }
 
