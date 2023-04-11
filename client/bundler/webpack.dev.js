86988147f4e0cb5fb6e48e3271e4cb135ea36923
dillabadu2138 <dillabadu2138@gmail.com>
2023-04-11 17:30:50 +0900

    create a client-side container

--- client/bundler/webpack.dev.js
+++ client/bundler/webpack.dev.js
@@ -6,6 +6,8 @@ module.exports = merge(commonConfiguration, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
+    host: '0.0.0.0',
+    port: 3000,
     static: {
       directory: path.join(__dirname, '../dist'),
     },
